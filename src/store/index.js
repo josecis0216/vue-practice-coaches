c1c1d8b12f153b58a8a28e80b6a8db2915a26c0a
josecis0216 <josecis0216@gmail.com>
2022-08-03 08:59:52 -0600

    starting user auth

--- src/store/index.js
+++ src/store/index.js
@@ -2,27 +2,19 @@ import { createStore } from 'vuex';
 
 import coachesModule from './coaches/coaches.js';
 import requestsModule from './requests/requests.js';
+import authModule from './auth/index.js';
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
-        reqs: requestsModule
-    }, 
-    state() {
-        return {
-            userId: 'c3'
-        };
+        reqs: requestsModule,
+        auth: authModule
     }, 
     mutations: {
 
     },
     actions: {
 
-    }, 
-    getters: {
-        userId(state) {
-            return state.userId;
-        }
     }
 });
 
