0ac37e491adbda879ab941e9e148d2e96d7d5570
josecis0216 <josecis0216@gmail.com>
2022-08-04 19:10:52 -0600

    async components added

--- src/router.js
+++ src/router.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachRegister from './pages/coaches/CoachRegister.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
 
 const router = createRouter({
     history: createWebHistory(),
