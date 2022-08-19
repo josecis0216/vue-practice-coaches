be99771ff644e6af23f6ca6208377d29cb3337e5
josecis0216 <josecis0216@gmail.com>
2022-08-19 09:02:57 -0600

    deployed site to firebase

--- src/router.js
+++ src/router.js
@@ -2,14 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachesList from './pages/coaches/CoachesList.vue';
 // import CoachDetail from './pages/coaches/CoachDetail.vue';
-import CoachRegister from './pages/coaches/CoachRegister.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import CoachRegister from './pages/coaches/CoachRegister.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
 
 const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegister = () => import('./pages/coaches/CoachRegister.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
 
 const router = createRouter({
     history: createWebHistory(),
